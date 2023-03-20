d28d1ac43ec175f8b04a7b1fe83ae35e2366625a
Ardikaas <ardikaajisetiawan@gmail.com>
2023-03-20 20:13:27 +0700

    menambahkan function search queue

--- Main.cpp
+++ Main.cpp
@@ -1,74 +1,29 @@
 #include <iostream>
 using namespace std;
+#define max 10
 
-float total;
-char operasi;
-int x;
-int y;
-int i;
+struct queue{
+  int data[max];
+  int head = 0;
+  int tail = 0;
+}queue;
 
-void penjumlahan(float a, float b){
-  cout << a << " + " << b << endl; 
-  total = a + b;
+void searchQue(){
+  int input;
+  cout << "masukkan data yang ingin dicari : ";
+  cin >> input;
+  for (int i = 0;i < max;i++){
+    if (queue.data[i] == input){
+      cout << "data" << input << "berada pada antrian ke " << i+1 << endl;
+    }
+  }
 }
-void pengurangan(float a, float b){
-  cout << a << " - " << b << endl; 
-  total = a - b;
-}
-void perkalian(float a, float b){
-  cout << a << " * " << b << endl; 
-  total = a * b;
-}
-void pembagian(float a, float b){
-  cout << a << " / " << b << endl; 
-  total = a / b;
-}
+
+
+
 
 int main(int argc, char const *argv[])
 {
-  cout << "Selamat Datang di Aplikasi Kalkulator" << endl;
-  cout << "\nPerhatian!!!" << endl;
-  cout << "Masukkan \"+\" untuk operasi penjumlahan" << endl;
-  cout << "Masukkan \"-\" untuk operasi pengurangan" << endl;
-  cout << "Masukkan \"*\" untuk operasi perkalian" << endl;
-  cout << "Masukkan \"/\" untuk operasi pembagian" << endl;
-  cout << "\nMasukkan bilangan pertama\t: ";
-  cin >> x;
-  cout << "Masukkan jenis operasi\t\t: ";
-  cin >> operasi;
-  i = 0;
-  while (i < 1){
-    if (operasi == '+' || operasi == '-'|| operasi == '*' || operasi == '/'){
-      i++;
-    }else{
-      system("cls");
-      cout << "\nOperasi yang anda masukkan salah!!!" << endl;
-      cout << "--------------------------------------" << endl;
-      cout << "\nPerhatian!!!" << endl;
-      cout << "Masukkan \"+\" untuk operasi penjumlahan" << endl;
-      cout << "Masukkan \"-\" untuk operasi pengurangan" << endl;
-      cout << "Masukkan \"*\" untuk operasi perkalian" << endl;
-      cout << "Masukkan \"/\" untuk operasi pembagian" << endl;
-      cout << "Masukkan bilangan pertama\t: " << x << endl;
-      cout << "Masukkan jenis operasi\t\t: ";
-      cin >> operasi;
-    }
-  }
-  cout << "Masukkan bilagan kedua\t\t: ";
-  cin >> y;
-  cout << "\n";
-
-  if (operasi == '+'){
-    penjumlahan(x, y);
-  }else if (operasi == '-'){
-    pengurangan(x, y);
-  }else if (operasi == '*'){
-    perkalian(x, y);
-  }else if (operasi == '/'){
-    pembagian(x, y);
-  }
-  cout << "\nTotal : " << total << endl;
-  cin.get();
-  cin.get();
+  
   return 0;
 }
